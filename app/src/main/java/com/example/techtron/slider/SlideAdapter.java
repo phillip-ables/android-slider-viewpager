259f36b32d99ed2c8e834ffd9f457a517b1fd034
Phillip <phillipables1@gmail.com>
2018-03-24 12:56:03 -0400

    better pngs set to int array for image list and a string array for title list

--- app/src/main/java/com/example/techtron/slider/SlideAdapter.java
+++ app/src/main/java/com/example/techtron/slider/SlideAdapter.java
@@ -1,10 +1,28 @@
 package com.example.techtron.slider;
 
+import android.content.Context;
 import android.support.v4.view.PagerAdapter;
+import android.view.LayoutInflater;
 import android.view.View;
 
 public class SlideAdapter extends PagerAdapter{
+    Context context;
+    LayoutInflater inflater;
 
+    //list of images
+    public int[] list_images = {
+            R.drawable.spacewale,
+            R.drawable.has_gravity,
+            R.drawable.heavy,
+            R.drawable.the_bat
+    };
+    //list of string titles
+    public String[] list_title = {
+            "WHAL-E",
+            "HAS GRAVITY",
+            "TO INFINITY",
+            "& BEYOND"
+    }
     @Override
     public int getCount() {
         return 0;
