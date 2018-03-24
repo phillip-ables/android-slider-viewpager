3b56d7495bb1b66594709ff2b49983d8cf6323db
Phillip <phillipables1@gmail.com>
2018-03-24 13:27:29 -0400

    add two override methods to slide adapter java, one to instantiateItem and the other to destroyItem

--- app/src/main/java/com/example/techtron/slider/SlideAdapter.java
+++ app/src/main/java/com/example/techtron/slider/SlideAdapter.java
@@ -5,6 +5,7 @@ import android.graphics.Color;
 import android.support.v4.view.PagerAdapter;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.LinearLayout;
 
 public class SlideAdapter extends PagerAdapter{
@@ -48,5 +49,15 @@ public class SlideAdapter extends PagerAdapter{
     @Override
     public boolean isViewFromObject(View view, Object object) {
         return (view == (LinearLayout)object);
+    }
+
+    @Override
+    public Object instantiateItem(ViewGroup container, int position) {
+        return super.instantiateItem(container, position);
+    }
+
+    @Override
+    public void destroyItem(ViewGroup container, int position, Object object) {
+        super.destroyItem(container, position, object);
     }
 }
